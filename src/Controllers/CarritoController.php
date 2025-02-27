a46ffe4878fc28fad635d42fca0fc6ddab10cfea
jairocod3000 <jairoalejandro9002@gmail.com>
2025-02-27 10:48:02 +0100

    Controladores de Carrito y Error creados

---
+++ src/Controllers/CarritoController.php
@@ -0,0 +1,127 @@
+<?php
+namespace Controllers;
+use Models\Producto;
+use Models\Categoria;
+use Lib\Pages;
+use Services\ProductoService;
+use Repositories\ProductoRepository;
+
+/**
+ * Clase CarritoController
+ *
+ * Esta clase maneja la lógica para las acciones relacionadas con el carrito.
+ */
+class CarritoController {
+    private Producto $producto;
+    private Categoria $categoria;
+    private Pages $pages;
+    private ProductoService $productoService;
+
+    /**
+     * Constructor de CarritoController.
+     *
+     * Inicializa una nueva instancia de la clase CarritoController.
+     */
+    public function __construct() {
+        $this->productoService = new ProductoService(new ProductoRepository());
+        $this->producto = new Producto();
+        $this->categoria = new Categoria();
+        $this->pages = new Pages();
+    }
+
+    /**
+     * Agrega un producto al carrito.
+     *
+     * @param int $id El ID del producto.
+     */
+    public function agregarProducto() {
+        $productoId = $_GET['id'];
+        $this->producto->setId($productoId);
+        $producto = $this->productoService->categoriaPorId($productoId);
+        if ($producto) {
+            $_SESSION['carrito'][$productoId] = $producto;
+            $_SESSION['carrito'][$productoId]['cantidad'] = 1;
+        }
+        header('Location: '.BASE_URL.'carrito/obtenerCarrito/');        
+    }
+
+    /**
+     * Elimina un producto del carrito.
+     *
+     * @param int $id El ID del producto.
+     */
+    public function eliminarProducto($id) {
+        $productoId = $id;
+        if (isset($_SESSION['carrito'][$productoId])) {
+            unset($_SESSION['carrito'][$productoId]);
+        }
+        header('Location: '.BASE_URL.'carrito/obtenerCarrito/');        
+    }
+
+    /**
+     * Obtiene los productos del carrito.
+     *
+     * @return array Los productos obtenidos.
+     */
+    public function obtenerCarrito() {
+        if (!isset($_SESSION['carrito'])) {
+            $_SESSION['carrito'] = [];
+        }
+        $productos = $_SESSION['carrito'];
+        $total = $this->obtenerTotalCarrito();
+        $this->pages->render('carrito/ver', ['productos' => $productos, 'total' => $total]);
+    }
+
+    /**
+     * Vacía el carrito.
+     */
+    public function vaciarCarrito() {
+        unset($_SESSION['carrito']);
+    }
+
+    /**
+     * Aumenta la cantidad de un producto en el carrito.
+     *
+     * @param int $id El ID del producto.
+     */
+    public function aumentarCantidad($id) {
+        $productoId = $id;
+        if (isset($_SESSION['carrito'][$productoId])) {
+            $_SESSION['carrito'][$productoId]['cantidad']++;
+        }
+        header('Location: '.BASE_URL.'carrito/obtenerCarrito/');        
+    }
+
+    /**
+     * Disminuye la cantidad de un producto en el carrito.
+     *
+     * @param int $id El ID del producto.
+     */
+    public function disminuirCantidad($id) {
+        $productoId = $id;
+        if (isset($_SESSION['carrito'][$productoId])) {
+            $_SESSION['carrito'][$productoId]['cantidad']--;
+            if ($_SESSION['carrito'][$productoId]['cantidad'] == 0) {
+                unset($_SESSION['carrito'][$productoId]);
+            }
+        }
+        header('Location: '.BASE_URL.'carrito/obtenerCarrito/');        
+
+    }
+
+    /**
+     * Obtiene el total del carrito.
+     *
+     * @return float El total del carrito.
+     */
+    public function obtenerTotalCarrito() {
+        $total = 0;
+        if (isset($_SESSION['carrito'])) {
+            foreach ($_SESSION['carrito'] as $producto) {
+                $total += $producto['precio'] * $producto['cantidad'];
+            }
+        }
+        return $total;
+    }
+
+}
--- src/Routes/Routes.php
+++ src/Routes/Routes.php
@@ -2,6 +2,12 @@
 namespace Routes;
 
 use Controllers\InicioController;
+use Controllers\CategoriaController;
+use Controllers\ProductoController;
+use Controllers\UsuarioController;
+use Controllers\CarritoController;
+use Controllers\ErrorController;
+use Controllers\PedidoController;
 use Lib\Router;
 
 class Routes {
@@ -10,5 +16,166 @@ class Routes {
             return (new InicioController())->index();
         });
 
+        Router::añadirRuta('GET', '/categoria/ver/?id=:id', function($id) {
+            return (new CategoriaController())->ver($id);
+        });
+
+        Router::añadirRuta('GET', '/categoria/crear', function() {
+            return (new CategoriaController())->crear();
+        });
+
+        Router::añadirRuta('POST', '/categoria/crear', function() {
+            return (new CategoriaController())->crear();
+        });
+
+        Router::añadirRuta('GET', '/categoria/borrar/?id=:id', function($id) {
+            return (new CategoriaController())->borrar($id);
+        });
+
+        Router::añadirRuta('GET', '/categoria/editar/?id=:id', function($id) {
+            return (new CategoriaController())->editar($id);
+        });
+
+        Router::añadirRuta('POST', '/categoria/actualizar', function() {
+            return (new CategoriaController())->actualizar();
+        });
+
+        Router::añadirRuta('GET', '/categoria/gestionarCategorias', function() {
+            return (new CategoriaController())->gestionarCategorias();
+        });
+
+        Router::añadirRuta('GET', '/producto/verDetalles/?id=:id', function($id) {
+            return (new ProductoController())->verDetalles($id);
+        });
+
+        Router::añadirRuta('GET', '/producto/crear', function() {
+            return (new ProductoController())->crear();
+        });
+
+        Router::añadirRuta('POST', '/producto/crear', function() {
+            return (new ProductoController())->crear();
+        });
+
+        Router::añadirRuta('GET', '/producto/borrar/?id=:id', function($id) {
+            return (new ProductoController())->borrar($id);
+        });
+
+        Router::añadirRuta('GET', '/producto/editar/?id=:id', function($id) {
+            return (new ProductoController())->editar($id);
+        });
+
+        Router::añadirRuta('POST', '/producto/editar', function() {
+            return (new ProductoController())->editar();
+        });
+
+        Router::añadirRuta('GET', '/producto/gestionarProductos', function() {
+            return (new ProductoController())->gestionarProductos();
+        });
+
+        Router::añadirRuta('GET', '/carrito/agregarProducto/?id=:id', function($id) {
+            return (new CarritoController())->agregarProducto($id);
+        });
+
+        Router::añadirRuta('GET', '/carrito/obtenerCarrito', function() {
+            return (new CarritoController())->obtenerCarrito();
+        });
+
+        Router::añadirRuta('GET', '/carrito/eliminarProducto/?id=:id', function($id) {
+            return (new CarritoController())->eliminarProducto($id);
+        });
+
+        Router::añadirRuta('GET', '/carrito/aumentarCantidad/?id=:id', function($id) {
+            return (new CarritoController())->aumentarCantidad($id);
+        });
+
+        Router::añadirRuta('GET', '/carrito/disminuirCantidad/?id=:id', function($id) {
+            return (new CarritoController())->disminuirCantidad($id);
+        });
+
+        Router::añadirRuta('GET', '/pedido/crear', function() {
+            return (new PedidoController())->crear();
+        });
+
+        Router::añadirRuta('GET', '/pedido/mostrarPedido', function() {
+            return (new PedidoController())->mostrarPedido();
+        }); 
+
+        Router::añadirRuta('GET', '/pedido/misPedidos', function() {
+            return (new PedidoController())->misPedidos();
+        });
+
+        Router::añadirRuta('GET', '/pedido/todosLosPedidos', function() {
+            return (new PedidoController())->todosLosPedidos();
+        });
+
+        Router::añadirRuta('GET', '/pedido/crear', function() {
+            return (new PedidoController())->crear();
+        });
+
+        Router::añadirRuta('POST', '/pedido/crear', function() {
+            return (new PedidoController())->crear();
+        });
+
+        Router::añadirRuta('GET', '/pedido/confirmarPedido/?id=:id', function($id) {
+            return (new PedidoController())->confirmarPedido($id);
+        });
+
+        Router::añadirRuta('GET', '/pedido/eliminar/?id=:id', function($id) {
+            return (new PedidoController())->eliminar($id);
+        });
+
+        Router::añadirRuta('GET', '/pedido/editar/?id=:id', function($id) {
+            return (new PedidoController())->editar($id);
+        });
+
+        Router::añadirRuta('GET', '/pedido/actualizar', function() {
+            return (new PedidoController())->actualizar();
+        });
+
+        Router::añadirRuta('POST', '/pedido/actualizar', function() {
+            return (new PedidoController())->actualizar();
+        });
+
+        Router::añadirRuta('GET', '/usuario/login', function() {
+            return (new UsuarioController())->login();
+        });   
+
+        Router::añadirRuta('POST', '/usuario/login', function() {
+            return (new UsuarioController())->login();
+        });
+
+        Router::añadirRuta('POST', '/usuario/registro', function() {
+            return (new UsuarioController())->registro();
+        });
+
+        Router::añadirRuta('GET', '/usuario/registro', function() {
+            return (new UsuarioController())->registro();
+        });
+
+        Router::añadirRuta('GET', '/usuario/verTodos', function() {
+            return (new UsuarioController())->verTodos();
+        });
+
+        Router::añadirRuta('GET', '/usuario/logout', function() {
+            return (new UsuarioController())->logout();
+        });
+
+        Router::añadirRuta('GET', '/usuario/eliminar/?id=:id', function($id) {
+            return (new UsuarioController())->eliminar($id);
+        });
+
+        Router::añadirRuta('GET', '/usuario/editar/?id=:id', function($id) {
+            return (new UsuarioController())->editar($id);
+        });
+
+        Router::añadirRuta('POST', '/usuario/actualizar', function() {
+            return (new UsuarioController())->actualizar();
+        });
+
+        Router::añadirRuta('GET', '/error', function() {
+            return (new ErrorController())->error404();
+        });
+
+        Router::dispatch();
     }
 }
